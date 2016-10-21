cb0985dcacc4d2f2b158639ee4e8804c10428b29
iRuxu <17229398@qq.com>
2016-10-22 00:35:59 +0800

    Signed-off-by: iRuxu <17229398@qq.com>

--- fixSidebar.js
+++ fixSidebar.js
@@ -2,16 +2,22 @@
 * fixSidebar v0.5
 * fix the sidebar when window scroll
 * https://github.com/iRuxu
-* update 2015.7.14
+* update 2016.5.13
 ------------------------------------------------------*/
 //args:
 //selector: #target 
 //top + bottom: margin to window for fix header or footer
 //triggerScroll : while scroll distance bigger than this and trigger the fix state
-(function($){ 
+
+jQuery(function($){
 
 	//定义
 	function fixSidebar(selector, top, bottom, triggerScroll) {
+
+		var $ = jQuery
+
+		//移动端不触发
+		if(isMobile) return 
 
 		//无参数时返回空
 		if (selector == undefined) return
@@ -24,70 +30,77 @@
 		if (bottom == undefined) bottom = 0
 		if (triggerScroll == undefined) triggerScroll = 20
 
-		//当内容高度需要高于窗口实际高度较多时才执行函数
-		if ($("body").height() < $(window).height()) return
+		//定义获取水平坐标函数
+		function getLeft(selector) {
+			var offset = selector.offsetLeft;
+			if (selector.offsetParent != null) offset += getLeft(selector.offsetParent);
+			return offset;
+		}
 
+		//设置恢复定位
+		function ActionPS() {
+			$(selector).css('position', 'static').removeClass('fixSidebar')
+		}
 
 		//获取相关尺寸与滚动发生距离
 		$(window).scroll(function() {
-			
-			//定义获取水平坐标函数
-			function getLeft(selector) {
-				var offset = selector.offsetLeft;
-				if (selector.offsetParent != null) offset += getLeft(selector.offsetParent);
-				return offset;
-			}
 
 			//获取尺寸
-			var scroll = $(window).scrollTop(), //滚动数值
-				screen_H = $(window).height(), //屏幕高度
-				bar_H = $(selector).height(), //侧边栏高度
-				ct_H = screen_H - top - bottom, //可视内容区高度（除去网站的fix头与底）
-				bar_X = getLeft($(selector)[0]), //侧边栏的水平位置
-				bar_Y = 0; //初始化侧边栏距顶
+			var scroll = $(window).scrollTop(), 	//滚动数值
+				screen_H = $(window).height(), 		//屏幕高度
+				body_H = $("body").height(),		//页面高度
+				bar_H = $(selector).height(), 		//侧边栏高度
+				ct_H = $("body").height() - top - bottom; 	//内容区可视区间高度
+				bar_X = getLeft($(selector)[0]), 	//侧边栏的水平位置
+				bar_Y = 0; 							//初始化侧边栏距顶
 
-			//设置恢复定位
-			function ActionPS() {
-				$(selector).css('position', 'static')
+			//如果内容区过短，即侧边栏与body长度约相等时，则暂时不执行fix
+			if(ct_H - bar_H < 100){
+				return
 			}
-
-			//当侧边栏高度小于可视区高度时，则以侧边栏顶部顶住上方
-			if (bar_H < ct_H) {
+			
+			//当内容区还可以被拖动时
+			if ( ct_H - scroll > screen_H) {
 				//设置侧边栏距顶为fix头的高度
-				bar_Y = top;
+				bar_Y = top
 				//当滚动值
 				if (scroll > triggerScroll) {
 					$(selector).css({
 						'position': 'fixed',
 						'left': bar_X,
-						'top': bar_Y
-					})
+						'top': bar_Y,
+						'bottom': ''
+					}).addClass('fixSidebar')
 				} else {
 					ActionPS()
 				}
-				//当侧边栏高度大于内容区时，则要以底部抵住下方为参考
 			} else {
 				//设置侧边栏距底为底部通栏的高度
-				bar_Y = -(bar_H - ct_H);
-				if (scroll + screen_H > bar_H && scroll > triggerScroll) {
+				bar_Y = bottom
+				if (scroll > bar_H - ct_H && scroll > triggerScroll) {
 					$(selector).css({
-						'position': 'relative',
-						'top': bar_Y
-					})
+						'position': 'fixed',
+						'left': bar_X,
+						'bottom': bar_Y,
+						'top': ''
+					}).addClass('fixSidebar')
 				} else {
 					ActionPS()
 				}
 			}
 		});
 	}
+
+	window.fixSidebar = fixSidebar
+
 	//触发事件
 	window.onload = function() {
-		$(window).trigger('scroll');
+		jQuery(window).trigger('scroll');
 	}
-	$(window).resize(function() {
-		$(window).trigger('scroll');
+	jQuery(window).resize(function() {
+		jQuery(window).trigger('scroll');
 	})
+		
+	window.fixSidebar = new fixSidebar('.default-sidebar', 46, 260 , 173);
 
-	window.fixSidebar = fixSidebar;
-
-})(jQuery);
+})
